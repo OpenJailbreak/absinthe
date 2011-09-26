a6b4a1523d8ea6a8efc49aaa69f11b6914e0c1f0
Joshua Hill <posixninja@gmail.com>
2011-09-26 11:02:28 -0400

    cleaned up crashreport parser

--- src/crashreport.c
+++ src/crashreport.c
@@ -21,18 +21,75 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <plist/plist.h>
 #include <libimobiledevice/libimobiledevice.h>
 
+#include "debug.h"
 #include "crashreport.h"
 
 crashreport_t* crashreport_create() {
 	crashreport_t* report = (crashreport_t*) malloc(sizeof(crashreport_t));
-	if(report) {
+	if (report) {
 		memset(report, '\0', sizeof(crashreport_t));
 	}
 	return report;
 }
 
 void crashreport_free(crashreport_t* report) {
+	if (report) {
+		free(report);
+	}
+}
 
+arm_state_t* crashreport_parse_state(const char* description) {
+	return NULL;
+}
+
+dylib_info_t** crashreport_parse_dylibs(const char* description) {
+	return NULL;
+}
+
+thread_info_t** crashreport_parse_threads(const char* description) {
+	return NULL;
+}
+
+crashreport_t* crashreport_parse_plist(plist_t plist) {
+	char* description = NULL;
+	plist_t description_node = NULL;
+	crashreport_t* crashreport = NULL;
+
+	// The description element is the one with all the good stuff
+	description_node = plist_dict_get_item(plist, "description");
+	if (description_node && plist_get_node_type(description_node) == PLIST_STRING) {
+		plist_get_string_val(description_node, &description);
+
+		crashreport = crashreport_create();
+		if(crashreport == NULL) {
+			error("Unable to allocate memory for crashreport\n");
+			return NULL;
+		}
+
+		crashreport->state = crashreport_parse_state(description);
+		if(crashreport->state == NULL) {
+			error("Unable to parse ARM state from crashreport\n");
+			crashreport_free(crashreport);
+			return NULL;
+		}
+
+		crashreport->dylibs = crashreport_parse_dylibs(description);
+		if(crashreport->dylibs == NULL) {
+			error("Unable to parse dylib base addresses from crashreport\n");
+			crashreport_free(crashreport);
+			return NULL;
+		}
+
+		crashreport->threads = crashreport_parse_threads(description);
+		if(crashreport->threads == NULL) {
+			error("Unable to parse thread info from crashreport\n");
+			crashreport_free(crashreport);
+			return NULL;
+		}
+	}
+
+	return crashreport;
 }
--- src/crashreport.h
+++ src/crashreport.h
@@ -20,12 +20,50 @@
 #ifndef CRASHREPORT_H_
 #define CRASHREPORT_H_
 
-typedef struct crashreport_t {
+#include <plist/plist.h>
 
+ typedef struct dylib_info {
+	const char* name;
+	uint32_t offset;
+} dylib_info_t;
+
+typedef struct arm_state_t {
+	uint32_t r0;
+	uint32_t r1;
+	uint32_t r2;
+	uint32_t r3;
+	uint32_t r4;
+	uint32_t r5;
+	uint32_t r6;
+	uint32_t r7;
+	uint32_t r8;
+	uint32_t r9;
+	uint32_t r10;
+	uint32_t r11;
+	uint32_t ip;
+	uint32_t sp;
+	uint32_t lr;
+	uint32_t pc;
+	uint32_t cpsr;
+} arm_state_t;
+
+typedef struct thread_info_t {
+	void* x;  // TODO: Anyone?
+} thread_info_t;
+
+/* The actual crashreport object containing it's data */
+typedef struct crashreport_t {
+	arm_state_t* state;
+	dylib_info_t** dylibs;
+	thread_info_t** threads;
 } crashreport_t;
 
 crashreport_t* crashreport_create();
 void crashreport_free(crashreport_t* report);
 
+crashreport_t* crashreport_parse_plist(plist_t crash);
+arm_state_t* crashreport_parse_state(const char* description);
+dylib_info_t** crashreport_parse_dylibs(const char* description);
+thread_info_t** crashreport_parse_threads(const char* description);
 
 #endif /* CRASHREPORT_H_ */
