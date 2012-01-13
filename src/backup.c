4fbe7331c7041c9652dfa7cc6d0d36111ee06c26
Nikias Bassen <nikias@gmx.li>
2012-01-13 13:49:09 +0100

    fix invalid free

--- src/backup.c
+++ src/backup.c
@@ -45,7 +45,6 @@ backup_t* backup_open(const char* backupdir, const char* uuid)
 		printf("Manifest.mbdb opened, %d records\n", mbdb->num_records);
 	} else {
 		fprintf(stderr, "ERROR: could not open %s\n", mbdb_path);
-		free(mbdb_path);
 		return NULL;
 	}
 
