c0965c99c9eaa8bad36240e90625984f728a48cb
Nikias Bassen <nikias@gmx.li>
2012-01-19 06:23:00 +0100

    Added missing files

---
+++ gui/iTunesKiller.cpp
@@ -0,0 +1,111 @@
+#if defined(__APPLE__) || defined(WIN32)
+#include "iTunesKiller.h"
+
+#include "debug.h"
+
+#if defined(__APPLE__)
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/sysctl.h>
+#include <sys/stat.h>
+#include <assert.h>
+#include <errno.h>
+#include <syslog.h>
+
+typedef struct kinfo_proc kinfo_proc;
+
+static int GetBSDProcessList(kinfo_proc **procList, size_t *procCount) /*{{{*/
+{
+	int err;
+	kinfo_proc* result;
+	bool done;
+	static const int name[] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
+	size_t length;
+
+	assert(procList != NULL);
+	assert(*procList == NULL);
+	assert(procCount != NULL);
+
+	*procCount = 0;
+
+	result = NULL;
+	done = false;
+	do {
+		assert(result == NULL);
+
+		length = 0;
+		err = sysctl( (int *) name, (sizeof(name) / sizeof(*name)) - 1, NULL, &length, NULL, 0);
+		if (err == -1) {
+			err = errno;
+		}
+
+		if (err == 0) {
+			result = (kinfo_proc*)malloc(length);
+			if (result == NULL) {
+				err = ENOMEM;
+			}
+		}
+
+		if (err == 0) {
+			err = sysctl( (int *) name, (sizeof(name) / sizeof(*name)) - 1, result, &length, NULL, 0);
+			if (err == -1) {
+				err = errno;
+			}
+			if (err == 0) {
+				done = true;
+			} else if (err == ENOMEM) {
+				assert(result != NULL);
+				free(result);
+				result = NULL;
+				err = 0;
+			}
+		}
+	} while (err == 0 && ! done);
+
+	if (err != 0 && result != NULL) {
+		free(result);
+		result = NULL;
+	}
+	*procList = result;
+	if (err == 0) {
+		*procCount = length / sizeof(kinfo_proc);
+	}
+
+	assert( (err == 0) == (*procList != NULL) );
+
+	return err;
+} /*}}}*/
+#endif
+
+iTunesKiller::iTunesKiller(int* watchdog)
+	: wxThread(wxTHREAD_JOINABLE), watchit(watchdog)
+{
+	debug("ITK:init\n");
+}
+
+wxThread::ExitCode iTunesKiller::Entry(void)
+{
+	while (*(this->watchit)) {
+#if defined(__APPLE__)
+		size_t proc_count = 0;
+		kinfo_proc *proc_list = NULL;
+		GetBSDProcessList(&proc_list, &proc_count);
+		int i;
+		for (i = 0; i < proc_count; i++) {
+			if ((!strcmp((&proc_list[i])->kp_proc.p_comm, "iTunesHelper")) || (!strcmp((&proc_list[i])->kp_proc.p_comm, "iTunes"))) {
+				kill((&proc_list[i])->kp_proc.p_pid, SIGKILL);
+			}
+		}
+		free(proc_list);
+#endif
+#if defined(WIN32)
+		// TODO
+#endif
+		sleep(2);
+	}
+	debug("ITK:Exiting.\n");
+	return 0;
+}
+#endif
