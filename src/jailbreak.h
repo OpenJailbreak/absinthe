5356b622dc92eca31c91c1de625928a48e75fd34
Joshua Hill <posixninja@chronic-dev.com>
2012-01-20 10:45:44 -0500

    fixed the vpn trigger and webclip

--- src/jailbreak.h
+++ src/jailbreak.h
@@ -2,7 +2,7 @@
 #define __JAILBREAK_H
 
 #define CONNECTION_NAME "jailbreak"
-#define VPN_TRIGGER_ADDRESS "greenpois0n.com"
+#define VPN_TRIGGER_ADDRESS "absinthejb.com"
 #define BKPTMP "nirvana"
 #define AFCTMP "HackStore"
 
