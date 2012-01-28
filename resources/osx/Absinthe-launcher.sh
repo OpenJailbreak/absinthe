bf73a6c6c37e249c43f43dc509be4d68c9a8ef33
Nikias Bassen <nikias@gmx.li>
2012-01-28 03:12:40 +0100

    Added missing OSX launcher script

---
+++ resources/osx/Absinthe-launcher.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+C=/${0}
+C=${C%/*}
+
+case `sysctl -n kern.osversion |sed 's/[A-Z]/:/g' |cut -d ":" -f 1` in
+11|10)
+	exec "${C:-.}"/Absinthe-10.6
+	;;
+9)
+	exec "${C:-.}"/Absinthe-10.5
+	;;
+*)
+	exit 1
+	;;
+esac
+exit 0
