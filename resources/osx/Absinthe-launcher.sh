4ecae73227a445580c4159af0ebd2a43f103a637
Nikias Bassen <nikias@gmx.li>
2012-01-28 13:38:36 +0100

    OSX: fixed launcher script so running from command line also works

--- resources/osx/Absinthe-launcher.sh
+++ resources/osx/Absinthe-launcher.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-C=/${0}
-C=${C%/*}
+C=`dirname ${0}`
 
 case `sysctl -n kern.osversion |sed 's/[A-Z]/:/g' |cut -d ":" -f 1` in
 11|10)
-	exec "${C:-.}"/Absinthe-10.6
+	exec "${C}"/Absinthe-10.6
 	;;
 9)
-	exec "${C:-.}"/Absinthe-10.5
+	exec "${C}"/Absinthe-10.5
 	;;
 *)
 	exit 1
