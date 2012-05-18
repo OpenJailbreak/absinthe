75b69cf531793107aa34efab51e2f8188db8e270
Nikias Bassen <nikias@gmx.li>
2012-05-19 00:54:48 +0200

    refined some GUI strings

--- gui/AbsintheMainWnd.h
+++ gui/AbsintheMainWnd.h
@@ -9,9 +9,9 @@
 #define WND_WIDTH 480
 #define WND_HEIGHT 350
 
-#define INFO_LABEL_TEXT "Welcome to Absinthe iOS 5.0/5.0.1/5.1 untethered A5 jailbreak!\n\nPlease make a backup of your device before using this tool. We don't expect any issues, but we aren't responsible if anything happens."
+#define INFO_LABEL_TEXT "Welcome to Absinthe iOS 5.1.1 untethered jailbreak!\n\nPlease make a backup of your device before using this tool. We don't expect any issues, but we aren't responsible if anything happens."
 #define BTN_START_TEXT "Jailbreak"
-#define CREDITS_LABEL_TEXT "Chronic-Dev Absinthe © 2011-2012 Chronic-Dev Team.\n5.1 exploits by: @pod2g, @westbaer, @pimskeks, ...\n5.0.x exploits by: @pod2g, @planetbeing, @saurik, @pimskeks,\n@p0sixninja, @MuscleNerd, and @xvolks.\nArtwork by @iOPK. GUI by Hanéne Samara & @pimskeks."
+#define CREDITS_LABEL_TEXT "Chronic-Dev Absinthe © 2011-2012 Chronic-Dev Team.\n5.1.x exploits by: @pod2g, @planetbeing, @pimskeks, ...\n5.0.x exploits by: @pod2g, @planetbeing, @saurik, @pimskeks,\n@p0sixninja, @MuscleNerd, and @xvolks.\nArtwork by @iOPK. GUI by Hanéne Samara & @pimskeks."
 #define PAYPAL_LINK_TEXT "Support us (PayPal)"
 #define PAYPAL_LINK_URL "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DPFUPCEAYUD4L"
 #define GP_LINK_TEXT "http://greenpois0n.com/"
